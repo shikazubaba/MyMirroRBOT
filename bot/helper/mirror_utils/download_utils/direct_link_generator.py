5fc24279b40e71d10bed161e5c63e2823e0da31b
Dawn India <shuvam.dawn12345@gmail.com>
2022-08-01 03:31:48 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -72,16 +72,16 @@ def direct_link_generator(link: str):
         raise DirectDownloadLinkException(f'No Direct link function found for {link}')
 
 def zippy_share(url: str) -> str:
-    base_url = re.search('http.+.zippyshare.com', url).group()
+    base_url = re_search('http.+.zippyshare.com', url).group()
     response = requests.get(url)
     pages = BeautifulSoup(response.text, "html.parser")
     js_script = pages.find("div", style="margin-left: 24px; margin-top: 20px; text-align: center; width: 303px; height: 105px;").text
     if js_script is None:
         js_script = pages.find("div", style="margin-left: -22px; margin-top: -5px; text-align: center;width: 303px;").text
     try:
-        mtk = eval(re.findall(r"\+\((.*?).\+", js_script)[0] + " + 11")
-        uri1 = re.findall(r".href.=.\"/(.*?)/\"", js_script)[0]
-        uri2 = re.findall(r"\)\+\"/(.*?)\"", js_script)[0]
+        mtk = eval(re_findall(r"\+\((.*?).\+", js_script)[0] + " + 11")
+        uri1 = re_findall(r".href.=.\"/(.*?)/\"", js_script)[0]
+        uri2 = re_findall(r"\)\+\"/(.*?)\"", js_script)[0]
     except Exception as err:
         LOGGER.error(err)
         raise DirectDownloadLinkException("ERROR: Can't Generate direct link")
